79ba5aa2076d660789119e4c75666a00ad54d537
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-03-22 21:23:19 +0900

    docs: Add RoomDetail Url
    - `RoomDetail` 클래스 추가
    - `get_room` 메서드 추가
    - `get`, `put`, `delete` 메서드 추가

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.Rooms.as_view()),
+    path("<int:pk>/", views.RoomDetail.as_view()),
+    path("<int:pk>/photos/", views.RoomPhotos.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -55,3 +55,68 @@ class Rooms(APIView):
                 serializer.errors,
                 status = HTTP_400_BAD_REQUEST,
             )
+            
+class RoomDetail(APIView):
+    
+    def get_room(self, pk):
+        try:
+            room = Room.objects.get(pk = pk)
+        except Room.DoesNotExist:
+            raise NotFound("Room not Found.")
+        return room
+    
+    def get(self, request, pk):
+        room = self.get_room(pk)
+        serializer = serializers.RoomDetailSerializer(
+            room,
+            context = {"request": request},
+        )
+        return Response( serializer.data )
+    
+    def put(self, request, pk):
+        room = self.get_room(pk)
+        if room.owner != request.user:
+            raise PermissionDenied("You are not owner.")
+        if not request.user.is_authenticated:
+            raise NotAuthenticated("You are not authenticated.")
+        serializer = serializers.RoomDetailSerializer(
+            room,
+            data = request.data,
+            partial = True,
+        )
+        if serializer.is_valid(): # 먼저 room에 대한 유효성 검사 후, foreign key에 대한 유효성 검사하여 save 전에 작업 수행하도록.
+            if "category" in request.data:
+                category_pk = request.data.get("category")
+                try:
+                    category = Category.objects.get(pk = category_pk)
+                    if category.kind == Category.CategoryKindChoices.EXPERIENCE:
+                        raise ParseError("Category cannot be Experience.")
+                    room.category = category # 유효성 검사를 통과했으면 room의 category를 변경, 결국 save 때 반영되는 것을 이용하는 원리.
+                except Category.DoesNotExist:
+                    raise NotFound("Category not Found.")
+                
+            if "amenities" in request.data:
+                amenities = request.data.get("amenities")
+                try:
+                    room.amenities.clear() # 기존 amenities를 모두 지우고 시작
+                    for amenity_pk in amenities:
+                        amenity = Amenity.objects.get(pk = amenity_pk)
+                        room.amenities.add(amenity)
+                except Amenity.DoesNotExist:
+                    raise NotFound("Amenity not Found.")
+            
+            updated_room = serializer.save() # 최종 반영
+            return Response( serializers.RoomDetailSerializer(updated_room).data )
+        else:
+            return Response(
+                serializer.errors,
+                status = HTTP_400_BAD_REQUEST,
+            )
+    
+    def delete(self, request, pk):
+        room = self.get_room(pk)
+        if room.owner != request.user:
+            raise PermissionDenied("You are not owner.")
+        room.delete()
+        return Response(status = HTTP_204_NO_CONTENT)
+    
