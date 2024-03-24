300ab6daef2aff4fcc4d422781d06e148770e30e
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-03-24 12:34:24 +0900

    docs: update Media
    - 미디어 URL을 추가
    - 미디어 파일을 제공하기 위한 설정 변경
    - 사진 삭제 기능을 추가, 인증된 사용자만 삭제 가능하도록 property설정

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings # settings.py 파일을 불러옴
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/rooms/', include('rooms.urls')),
+    path('api/v1/medias/', include('medias.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 개발 단계에서 미디어 파일을 제공하기 위한 설정
--- medias/views.py
+++ medias/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
+from rest_framework.exceptions import NotFound, ParseError, PermissionDenied
+from .models import Photo
 
-# Create your views here.
+class PhotoDetail(APIView):
+    
+    permission_classes = [IsAuthenticated] # 인증 여부를 확인할 수 있는 property 설정, is_authenticated를 확인하지 않아도 된다.
+    
+    def get_object(self, pk):
+        try:
+            return Photo.objects.get(pk = pk)
+        except Photo.DoesNotExist:
+            raise NotFound("Photo not found.")
+    
+    def delete(self, request, pk):
+        photo = self.get_object(pk)
+        if photo.room: # 사진이 방을 가지고 있는지 확인
+            if photo.room.owner != request.user: # 방의 주인이 아닌 경우
+                raise PermissionDenied("You are not owner.")
+        elif photo.experience:
+            if photo.experience.host != request.user:
+                raise PermissionDenied("You are not owner.")
+        photo.delete()
+        return Response(status = HTTP_204_NO_CONTENT) # 8분13초
