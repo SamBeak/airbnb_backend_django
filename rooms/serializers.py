bc9752fc0986f6ca2eab19528072784bf6180423
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-03-26 22:06:13 +0900

    docs: Update room/serializers.py
    - rooms/serializers.py에 변경 내용이 추가
    - wishlist의 여부 확인할 수 있도록 추가

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -3,6 +3,7 @@ from .models import Room, Amenity
 from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
 from medias.serializers import PhotoSerializer
+from wishlists.models import Wishlist
 
 class AmenitySerializer(serializers.ModelSerializer):
     class Meta:
@@ -26,6 +27,7 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     )
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
     photos = PhotoSerializer(
         many = True,
         read_only = True,
@@ -41,6 +43,9 @@ class RoomDetailSerializer(serializers.ModelSerializer):
         if request: # request가 존재하면
             return room.owner == request.user
         return False
+    def get_is_liked(self, room):
+        request = self.context["request"] # 따라서, get을 사용하기보다 []를 사용하는게 에러 방지에 좋다.
+        return Wishlist.objects.filter(user = request.user, rooms__pk = room.pk).exists() # room의 pk가 있는 room list를 포함한 wishlist가 존재하는지 결과를 반환한다.
     
     
     
