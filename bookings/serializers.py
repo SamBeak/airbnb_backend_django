1565e9750c5e24a4ed3fb846a603b3cb1f307423
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-03-27 12:53:27 +0900

    docs: Update booking/serializers.py
    - booking serializer와 room view에 새로운 CreateRoomBookingSerializer 추가
    - custom validation 추가

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,5 +1,39 @@
+from django.utils import timezone
 from rest_framework import serializers
 from .models import Booking
+
+class CreateRoomBookingSerializer(serializers.ModelSerializer):
+        
+        check_in = serializers.DateField() # models에선 null=True, blank=True로 설정했지만 여기선 필수로 설정
+        check_out = serializers.DateField()
+        
+        class Meta:
+            model = Booking
+            fields = ( # user에게 받고 싶은 data를 정의
+                "check_in",
+                "check_out",
+                "guests",
+            )
+        def validate_check_in(self, value): # 특정 필드의 validation을 커스텀하는 방법, value는 사용자가 입력한 값
+            now = timezone.localtime(timezone.now()).date() # 현재 날짜
+            check_out = self.initial_data.get("check_out") # 사용자가 입력한 check_out 값
+            if now > value:
+                raise serializers.ValidationError("Check-in date must be future.")
+            if value >= check_out:
+                raise serializers.ValidationError("Check-out date must be later than check-in date.")
+            return value # return value를 하면 성공적으로 validation을 통과한 것으로 간주
+        
+        def validate_check_out(self, value): # 특정 필드의 validation을 커스텀하는 방법, value는 사용자가 입력한 값
+            now = timezone.localtime(timezone.now()).date() # 현재 날짜
+            check_in = self.initial_data.get("check_in") # 사용자가 입력한 check_in 값
+            if now > value:
+                raise serializers.ValidationError("Check-in date must be future.")
+            if value <= check_in:
+                raise serializers.ValidationError("Check-out date must be later than check-in date.")
+            return value # return value를 하면 성공적으로 validation을 통과한 것으로 간주
+        
+        
+        
 
 class PublicBookingSerializer(serializers.ModelSerializer):
     
--- rooms/views.py
+++ rooms/views.py
@@ -12,7 +12,7 @@ from medias.serializers import PhotoSerializer
 from reviews.serializers import ReviewSerializer
 from django.conf import settings
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 
 class Rooms(APIView):
     
@@ -214,3 +214,14 @@ class RoomBookings(APIView):
             many = True,
         )
         return Response( serializer.data )
+    
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data = request.data)
+        if serializer.is__valid():
+            pass
+        else:
+            return Response(
+                serializer.errors,
+                status = HTTP_400_BAD_REQUEST,
+            )
