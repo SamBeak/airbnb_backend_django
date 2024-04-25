6442995cfa9aec3695c94d2da3be35b5e510fbcc
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-04-25 18:46:47 +0900

    docs: Update serializers.py
     유저 로그인 여부 파악하도록 수정

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -45,8 +45,10 @@ class RoomDetailSerializer(serializers.ModelSerializer):
         return False
     def get_is_liked(self, room):
         request = self.context["request"] # 따라서, get을 사용하기보다 []를 사용하는게 에러 방지에 좋다.
-        return Wishlist.objects.filter(user = request.user, rooms__pk = room.pk).exists() # room의 pk가 있는 room list를 포함한 wishlist가 존재하는지 결과를 반환한다.
-    
+        if request.user.is_authenticated:
+            return Wishlist.objects.filter(user = request.user, rooms__pk = room.pk).exists() # room의 pk가 있는 room list를 포함한 wishlist가 존재하는지 결과를 반환한다.
+        else:
+            return False
     
     
 class RoomListSerializer(serializers.ModelSerializer):
